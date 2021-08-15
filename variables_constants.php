4f7306052587587e9ed2b6f89b4638e550129b9e
Kevin Barreto <barretokevin@hotmail.com>
2021-08-14 22:17:00 -0500

    ways of defining a constant variable

---
+++ variables_constants.php
@@ -0,0 +1,23 @@
+<?php
+
+// variables
+$country = "Mexico";
+$age = 29;
+
+echo $country;
+echo "<br />";
+echo $age;
+
+// CONSTANT variables #1
+const NAME = 'Kevin';
+echo "<br />";
+echo NAME;
+
+echo "<br />";
+
+// CONSTANT variables #2
+define('LAST_NAME', 'Barreto');
+echo LAST_NAME;
+
+
+
