11e580b65bae058bb3c1642e50d73c2c012723e8
Kevin Barreto <barretokevin@hotmail.com>
2021-08-14 22:01:24 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $name = "Kevin";
+    $last_name = "Barreto";
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Course</title>
+</head>
+<body>
+    <h1>My name is: <?php echo $name . " " . $last_name ?></h1>
+</body>
+</html>
